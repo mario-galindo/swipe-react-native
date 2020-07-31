0712ccb1c2674eb263584d002163ed6146f86abb
mario-galindo <mgalindo9410@gmail.com>
2020-07-31 13:14:52 -0600

    initial setting

--- App.js
+++ App.js
@@ -1,21 +1,166 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
+import { SwipeListView } from "react-native-swipe-list-view";
+import Notifications from "./Notifications";
 
-export default function App() {
+const App = () => {
+  const [listData, setListData] = useState(
+    Notifications.map((NotificationItem, index) => ({
+      key: `${index}`,
+      title: NotificationItem.title,
+      details: NotificationItem.details,
+    }))
+  );
+
+  const closeRow = (rowMap,rowKey) => {
+    
+  }
+
+  const deleteRow = () => {
+
+  }
+
+  const VisibleItem = (props) => {
+    const { data } = props;
+    return (
+      <View style={styles.rowFront}>
+        <TouchableHighlight style={styles.rowFrontVisible}>
+          <View>
+            <Text style={styles.title} numberOfLines={1}>
+              {data.item.title}
+            </Text>
+            <Text style={styles.details} numberOfLines={1}>
+              {data.item.details}
+            </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  };
+
+  const renderItem = (data, rowMap) => {
+    return <VisibleItem data={data} />;
+  };
+
+  const HiddenItemWithActions = props => {
+    const {onClose,onDelete} = props;
+    return(
+      <View style={styles.rowBack}>
+        <Text>Left</Text>
+        <TouchableOpacity style={[styles.backRightBtn,styles.backRightBtnLeft]}>
+          <Text>Close</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.backRightBtn,styles.backRightBtnRight]}>
+          <Text>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  const renderHiddenItem = (data, rowMap) => {
+    return (
+      <HiddenItemWithActions
+        data={data}
+        rowMap={rowMap}
+        onClose={() => closeRow(rowMap, data.item.key)}
+        onDelete={() => deleteRow(rowMap, data.item.key)}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SwipeListView
+        data={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={75}
+        rightOpenValue={-150}
+      />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#f4f4f4",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  backTextWhite: {
+    color: "#FFF",
+  },
+  rowFront: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    height: 60,
+    margin: 5,
+    marginBottom: 15,
+    shadowColor: "#999",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  rowFrontVisible: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    height: 60,
+    padding: 10,
+    marginBottom: 15,
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#DDD",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15,
+    margin: 5,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  backRightBtn: {
+    alignItems: "flex-end",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 75,
+    paddingRight: 17,
+  },
+  backRightBtnLeft: {
+    backgroundColor: "#1f65ff",
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: "red",
+    right: 0,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  trash: {
+    height: 25,
+    width: 25,
+    marginRight: 7,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: "#666",
+  },
+  details: {
+    fontSize: 12,
+    color: "#999",
   },
 });
